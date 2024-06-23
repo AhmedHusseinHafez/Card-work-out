35c8a6da28437129249a8f04b35cbc8b8074fc13
Ahmed Hussein <hsino105@gmail.com>
2024-06-23 05:57:42 +0300

    Initial Commit

---
+++ CardWorkout/Card.swift
@@ -0,0 +1,73 @@
+//
+//  Card.swift
+//  CardWorkout
+//
+//  Created by Ahmed Husain on 23/06/2024.
+//
+
+import UIKit
+
+
+struct Card {
+    static var allValues = [
+    UIImage(named: "cards/2H")!,
+    UIImage(named: "cards/3H")!,
+    UIImage(named: "cards/4H")!,
+    UIImage(named: "cards/5H")!,
+    UIImage(named: "cards/6H")!,
+    UIImage(named: "cards/7H")!,
+    UIImage(named: "cards/8H")!,
+    UIImage(named: "cards/9H")!,
+    UIImage(named: "cards/10H")!,
+    UIImage(named: "cards/JH")!,
+    UIImage(named: "cards/QH")!,
+    UIImage(named: "cards/KH")!,
+    UIImage(named: "cards/AH")!,
+ 
+    
+    UIImage(named: "cards/2S")!,
+    UIImage(named: "cards/3S")!,
+    UIImage(named: "cards/4S")!,
+    UIImage(named: "cards/5S")!,
+    UIImage(named: "cards/6S")!,
+    UIImage(named: "cards/7S")!,
+    UIImage(named: "cards/8S")!,
+    UIImage(named: "cards/9S")!,
+    UIImage(named: "cards/10S")!,
+    UIImage(named: "cards/JS")!,
+    UIImage(named: "cards/QS")!,
+    UIImage(named: "cards/KS")!,
+    UIImage(named: "cards/AS")!,
+
+    
+    
+    UIImage(named: "cards/2C")!,
+    UIImage(named: "cards/3C")!,
+    UIImage(named: "cards/4C")!,
+    UIImage(named: "cards/5C")!,
+    UIImage(named: "cards/6C")!,
+    UIImage(named: "cards/7C")!,
+    UIImage(named: "cards/8C")!,
+    UIImage(named: "cards/9C")!,
+    UIImage(named: "cards/10C")!,
+    UIImage(named: "cards/JC")!,
+    UIImage(named: "cards/QC")!,
+    UIImage(named: "cards/KC")!,
+    UIImage(named: "cards/AC")!,
+
+    UIImage(named: "cards/2D")!,
+    UIImage(named: "cards/3D")!,
+    UIImage(named: "cards/4D")!,
+    UIImage(named: "cards/5D")!,
+    UIImage(named: "cards/6D")!,
+    UIImage(named: "cards/7D")!,
+    UIImage(named: "cards/8D")!,
+    UIImage(named: "cards/9D")!,
+    UIImage(named: "cards/10D")!,
+    UIImage(named: "cards/JD")!,
+    UIImage(named: "cards/QD")!,
+    UIImage(named: "cards/KD")!,
+    UIImage(named: "cards/AD")!,
+   
+    ]
+}
---
+++ CardWorkout/CardSelectionVC.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    @IBOutlet var cardImageView: UIImageView!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
+    var cards: [UIImage] = Card.allValues
+    
+    var timer : Timer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startTimer()
+        for button in buttons {
+            button.layer.cornerRadius = 8
+        }
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+//        timer.invalidate()
+       
+    }
+    
+    func startTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage(){
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+    }
+
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+    }
+    
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        startTimer()
+    }
+}
